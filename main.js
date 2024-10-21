f90e3992eddfd671011b86750f19ddf42397cc20
BrazenTwo <tildo2001@gmail.com>
2024-10-21 19:39:29 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//Variable: es un espacio de memoria reservado para almacenar informacion importante del programa.
+
+//Declaracion:
+let cliente;
+
+//Asignacion de valores:
+cliente = "Matías";
+
+console.log(cliente)
+
+//Inicializar variables:
+
+let edad = 37;
+
+console.log(edad);
+
+//Const: es una variable que no se puede modificar una vez asignada.
+
+const nacimiento = 1987
+
+console.log(nacimiento)
+
+const titulo = document.getElementById("titulo");
+
+titulo.style.color = "red";
+titulo.style.fontSize = "25px";
